f4d148d12af81f9fa5e380817133f513330ffd2a
Thuong Nguyen <nguyenvanthuong0201@gmail.com>
2021-08-22 16:07:02 +0700

    edit,delete contacts

--- contacts.py
+++ contacts.py
@@ -18,7 +18,6 @@ class Contacts:
 
     def execute_db_query(self, query, parameters=()):
         with sqlite3.connect(self.db_filename) as conn:
-            print(conn)
             print("You have successfully connected to the database")
             cursor = conn.cursor()
             query_result = cursor.execute(query, parameters)
@@ -33,7 +32,6 @@ class Contacts:
         self.create_scrollbar()
         self.create_bottom_buttons()
         self.view_contacts()
-
 
 
     ### Tạo icon
@@ -58,7 +56,7 @@ class Contacts:
         self.numfield= Entry(labelFrame)
         self.numfield.grid(row=3, column=2, sticky=W, padx=5, pady=2)
 
-        Button(labelFrame, text="Add Contact", command=self.on_add_contactr_button_clicked, bg="blue", fg="white").grid(row=4, column=2, sticky=E, padx=5, pady=5)
+        Button(labelFrame, text="Add Contact", command=self.on_add_contact_button_clicked, bg="blue", fg="white").grid(row=4, column=2, sticky=E, padx=5, pady=5)
 
     ### Tạo message
     def create_message_area(self):
@@ -79,18 +77,64 @@ class Contacts:
         self.scrollbar.grid(row=6, column=3, rowspan=10, sticky='sn')
 
     def create_bottom_buttons(self):
-        Button(text='Delete Selected', command="", bg="red", fg="white").grid(row=8, column=0, sticky=W, pady=10, padx= 20)
-        Button(text='Modify Selected', command="", bg="purple", fg="white").grid(row=8, column=1, sticky=W)
+        Button(text='Delete Selected', command=self.on_delete_contact_button_clicker, bg="red", fg="white").grid(row=8, column=0, sticky=W, pady=10, padx= 20)
+        Button(text='Modify Selected', command=self.on_modify_selected_button_clicker, bg="purple", fg="white").grid(row=8, column=1, sticky=W)
 
-    def on_add_contactr_button_clicked(self):
+    def on_add_contact_button_clicked(self):
         self.add_new_contact()
+    def on_delete_contact_button_clicker(self):
+        self.message['text'] = ''
+        try:
+            self.tree.item(self.tree.selection())['values'][0]
+        except IndexError as e:
+            self.message['text'] = 'No item selected to delete'
+            return
+        self.delete_contact()
+
+    def on_modify_selected_button_clicker(self):
+        self.message['text'] = ''
+        try:
+            self.tree.item(self.tree.selection())['values'][0]
+        except IndexError as e:
+            self.message['text'] = 'No item selected to modify'
+            return
+        self.open_modify_window()
+
+    def open_modify_window(self):
+        name = self.tree.item(self.tree.selection())['text']
+        old_number = self.tree.item(self.tree.selection())['values'][1]
+        self.transient = Toplevel()
+        self.transient.title("Update Contact")
+        Label(self.transient, text="Name").grid(row=0, column=1)
+        Entry(self.transient, textvariable=StringVar(self.transient, value=name), state='readonly').grid(row=0, column=2)
+        Label(self.transient, text="Old contact number").grid(row=1, column=1)
+        Entry(self.transient, textvariable=StringVar(self.transient, value=old_number), state='readonly').grid(row=1, column=2)
+        Label(self.transient, text="New Phone Number:").grid(row=2, column=1)
+        new_phone_number = Entry(self.transient)
+        new_phone_number.grid(row=2, column=2)
+
+        Button(self.transient, text='Update Contact', command=lambda: self.update_contact(
+            new_phone_number.get(), old_number, name)).grid(row=3, column=2, sticky=E)
+
+        self.transient.mainloop()
+
+    def update_contact(self, new_phone, old_phone,name):
+        query = 'UPDATE contacts_list SET number=? WHERE number =? AND name =?'
+        parameters = (new_phone, old_phone, name)
+        self.execute_db_query(query, parameters)
+        self.transient.destroy()
+        self.message['text'] = 'Phone number of {} modified'.format(name)
+        self.view_contacts()
+
+
+
+
 
     def add_new_contact(self):
         if self.new_contact_validate():
             query = "INSERT INTO contacts_list VALUES(NULL, ?, ?, ?)"
             parameters = (self.namefield.get(), self.emailfield.get(), self.numfield.get())
             self.execute_db_query(query, parameters)
-            print(parameters)
             self.message['text'] = 'New contact {} added'.format(self.namefield.get())
             self.namefield.delete(0, END)
             self.emailfield.delete(0, END)
@@ -112,6 +156,14 @@ class Contacts:
         contact_entries = self.execute_db_query(query)
         for row in contact_entries:
             self.tree.insert('', 0, text=row[1], values=(row[2], row[3]))
+
+    def delete_contact(self):
+        self.message['text'] = ''
+        name = self.tree.item(self.tree.selection())['text']
+        query = "DELETE FROM contacts_list WHERE name =? "
+        self.execute_db_query(query, (name, ))
+        self.message['text']="Contacts for {} deleted".format(name)
+        self.view_contacts()
 
 if __name__ =='__main__':
     root = Tk()
